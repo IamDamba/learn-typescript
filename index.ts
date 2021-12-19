9ef9913a5e0ac4ed7b93d60288ad99eb0c03929b
iamdamba <iamdamba720@gmail.com>
2021-12-19 17:02:54 +0100

    First Commit Built-In-Types

---
+++ index.ts
@@ -0,0 +1,27 @@
+// // NUMBER YPE
+
+// let age = 23;
+// let weight: number = 177
+
+// weight = 140; //Correct
+// weight = true; // Incorrect
+// weight = "true"; // Incorrect
+
+// age = "Incorrect"; //Incorrect even if not specified
+
+// //  TYPE STRING
+
+// let myName = "Aris";
+// let dogName =: string = "Doggy";
+
+// myName = true; // Incorrect
+// myName = "true".length; // Incorrect
+
+
+// //  BOOLEAN STRING
+
+// let isAwesome = false; //Incorrect
+// let isAwesomeToo = "ko".length === 0; //Correct because 0 and 1 are boolean response
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
