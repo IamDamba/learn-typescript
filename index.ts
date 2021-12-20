6f61d2d2937f91d25651614e21d4d78815727f76
iamdamba <iamdamba720@gmail.com>
2021-12-20 15:34:40 +0100

    Inference vs Assignment

--- index.ts
+++ index.ts
@@ -1,27 +1,8 @@
-// // NUMBER YPE
+// let height = 160;
+// height = "200";
+// let height2: number = 120;
+// height2 = "";
 
-// let age = 23;
-// let weight: number = 177
-
-// weight = 140; //Correct
-// weight = true; // Incorrect
-// weight = "true"; // Incorrect
-
-// age = "Incorrect"; //Incorrect even if not specified
-
-// //  TYPE STRING
-
-// let myName = "Aris";
-// let dogName =: string = "Doggy";
-
-// myName = true; // Incorrect
-// myName = "true".length; // Incorrect
-
-
-// //  BOOLEAN STRING
-
-// let isAwesome = false; //Incorrect
-// let isAwesomeToo = "ko".length === 0; //Correct because 0 and 1 are boolean response
-
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
+// let amount: number | string = 120;
+// amount = 120;
+// amount = "true";
