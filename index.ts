fcdbebe7a868edd640d6e702dc1e95f1b82a3350
iamdamba <iamdamba720@gmail.com>
2021-12-20 15:44:09 +0100

    Object

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-// let height = 160;
-// height = "200";
-// let height2: number = 120;
-// height2 = "";
+const obj = {
+  firstName: "Aris",
+  lastName: "Dev",
+  number: "06000000",
+  age: 23,
+  gender: false
+};
 
-// let amount: number | string = 120;
-// amount = 120;
-// amount = "true";
+const obj2 : {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  age: number;
+  gender: boolean;
+} = {
+  firstName: "",
+  lastName: "",
+  phone: "",
+  age: 23,
+  gender: true
+}
+
+obj2.age = 34;
+obj2.age = true; //Incorrect
+
+obj2.name; //Incorrect, cause dion't recognize it within the object declaration
+
+obj2.phone.length
+obj2.age.length //Incorrect
